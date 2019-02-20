d984d1c92ba13030cf5795acb23011d4e765be10
Nguyen Duc Chung <chungnd@magenest.com>
2019-02-20 13:18:21 +0700

    updating

--- nodejs/src/client.js
+++ nodejs/src/client.js
@@ -6,8 +6,20 @@ const client = net.createConnection(6789, 'tinyos', () => {
 });
 client.on('data', (data) => {
   console.log(data.toString());
-  client.end();
+  // client.end();
 });
 client.on('end', () => {
   console.log('disconnected from server');
+});
+
+var app = require('http').createServer();
+var io = require('socket.io')(app);
+
+app.listen(9876);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
