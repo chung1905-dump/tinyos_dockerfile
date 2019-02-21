6a35142bc607608a94fd829554d33791af999912
Nguyen Duc Chung <chungnd@magenest.com>
2019-02-21 19:19:26 +0700

    better fake text and buffer

--- nodejs/src/client.js
+++ nodejs/src/client.js
@@ -1,25 +1,31 @@
+const strl = 53;
 const net = require('net');
-const client = net.createConnection(6789, 'tinyos', () => {
-  // 'connect' listener
-  console.log('connected to server!');
-  client.write('world!\r\n');
+const socket = net.createConnection(6789, 'tinyos', () => {
+    console.log('connected to server!');
+    // client.write('world!\r\n');
 });
-client.on('data', (data) => {
-  console.log(data.toString());
-  // client.end();
+var input = '';
+var hex = '';
+socket.on('data', (raw) => {
+    input = input + raw.toString();
+    if (input.length >= strl) {
+        hex = input.substr(0, strl);
+        console.log(hex);
+        input = input.substr(strl);
+    }
 });
-client.on('end', () => {
-  console.log('disconnected from server');
+socket.on('end', () => {
+    console.log('disconnected from server');
 });
 
-var app = require('http').createServer();
-var io = require('socket.io')(app);
-
-app.listen(9876);
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+// var app = require('http').createServer();
+// var io = require('socket.io')(app);
+//
+// app.listen(9876);
+//
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
--- tinyos/src/Listener.java
+++ tinyos/src/Listener.java
@@ -33,11 +33,24 @@ public class Listener {
 
     private static void runTestOutput(DataOutputStream outputStream) throws IOException {
         System.out.println("Sending fake output each 5 seconds");
-        Integer c = 0;
-        for (;;) {
-            outputStream.writeBytes(String.valueOf(c));
-            System.out.println(c);
-            c++;
+        String[] c = {
+            "00 FF FF 00 42 0A 00 01 0F CC 1D 3F 02 27 00 BD 01 6D",
+            "00 FF FF 00 42 0A 00 01 0F CB 1D 3F 02 16 01 40 02 0E",
+            "00 FF FF 00 42 0A 00 01 0F CD 1D 3C 02 19 01 55 01 C3",
+            "00 FF FF 00 42 0A 00 01 0F CC 1D 3F 02 1A 00 AB 01 3F",
+            "00 FF FF 00 42 0A 00 01 0F CB 1D 3F 02 1C 01 2B 01 F5",
+            "00 FF FF 00 42 0A 00 01 0F CC 1D 3E 02 1C 01 51 01 AF",
+            "00 FF FF 00 42 0A 00 01 0F CC 1D 3D 02 27 00 C4 01 78",
+            "00 FF FF 00 42 0A 00 01 0F CC 1D 3D 02 27 01 17 01 E4",
+            "00 FF FF 00 42 0A 00 01 0F CC 1D 3F 02 28 01 5D 01 EE",
+            "00 FF FF 00 42 0A 00 01 0F CD 1D 3E 02 22 00 FA 01 18"
+        };
+        for (int i = 0; i < c.length; i++) {
+            outputStream.writeBytes(String.valueOf(c[i]));
+            System.out.println(c[i]);
+            if (i == c.length) {
+                i = 0;
+            }
             try {
                 TimeUnit.SECONDS.sleep(5);
             } catch (java.lang.InterruptedException e) {
