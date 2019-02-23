2f90368e1e7307d406be2cd3d083d56f5b4e8234
chung <chung1905@gmail.com>
2019-02-23 17:20:45 +0700

    parsed data is sent to html

--- nodejs/src/client.js
+++ nodejs/src/client.js
@@ -2,23 +2,31 @@ const strl = 53;
 const net = require('net');
 const app = require('http').createServer();
 const io = require('socket.io')(app);
+const hexParser = require('./parseHex');
 
 var input = '';
 var hex = '';
+var parsedObj = '';
+
+var onTcpReceived = function (raw) {
+    input = input + raw.toString();
+    if (input.length >= strl) {
+        hex = input.substr(0, strl);
+        parsedObj = hexParser.parse(hex);
+        // parsedObj.time = Date().toLocaleString();
+
+        input = input.substr(strl);
+
+        io.emit('update', parsedObj);
+        // console.log(hex);
+        console.log(parsedObj);
+    }
+};
 
 const tcpSocket = net.createConnection(6789, 'tinyos', () => {
     console.log('connected to server!');
 
-    tcpSocket.on('data', (raw) => {
-        input = input + raw.toString();
-        if (input.length >= strl) {
-            hex = input.substr(0, strl);
-            console.log(hex);
-            input = input.substr(strl);
-
-            io.emit('update', {hex});
-        }
-    });
+    tcpSocket.on('data', onTcpReceived);
     tcpSocket.on('end', () => {
         console.log('disconnected from server');
     });
--- nodejs/src/parseHex.js
+++ nodejs/src/parseHex.js
@@ -1,67 +1,74 @@
+function parse(rawHex) {
+    var hex = splitHexStr(rawHex),
+        id, voltHex, tempHex, humHex, lightHex, tsrHex;
 
-function splitHexStr(hexVal) {
-    hex = hexVal.toUpperCase().match(/[0-9A-F]{2}/g);
-    return hex;
+    for (var i = 0; i < hex.length; i++) {
+        if (i === 3) {
+            id = hex[i] + hex[i + 1];
+        } else if (i === 8) {
+            voltHex = hex[i] + hex[i + 1];
+        } else if (i === 10) {
+            tempHex = hex[i] + hex[i + 1];
+        } else if (i === 12) {
+            humHex = hex[i] + hex[i + 1];
+        } else if (i === 14) {
+            lightHex = hex[i] + hex[i + 1];
+        } else if (i === 16) {
+            tsrHex = hex[i] + hex[i + 1];
+        }
+    }
+
+    var volt = calVolt(voltHex),
+        temp = calTemp(tempHex),
+        hum = calHum(humHex, temp),
+        light = calLight(lightHex),
+        tsr = calTSR(tsrHex);
+
+    return {
+        id,
+        volt: Math.round(volt * 100) / 100,
+        temp: Math.round(temp * 100) / 100,
+        hum: Math.round(hum * 100) / 100,
+        light: Math.round(light * 100) / 100,
+        tsr: Math.round(tsr * 100) / 100,
+    };
 }
 
-splitHexStr("00 FF FF 00 42 0A 00 01 0F CD 1D 3E 02 22 00 FA 01 18")
+function splitHexStr(hexVal) {
+    return hexVal.match(/[0-9A-F]{2}/ig);
+}
 
 function hexToDec(hex) {
     return hex.toLowerCase().split('').reduce((result, ch) =>
         result * 16 + '0123456789abcdefgh'.indexOf(ch), 0);
 }
 
-
-for (i = 0; i < hex.length; i++) {
-    if (i == 8) {
-        voltHex = hex[i] + hex[i + 1];
-    }
-    if (i == 10) {
-        tempHex = hex[i] + hex[i + 1];
-    }
-    if (i == 12) {
-        humHex = hex[i] + hex[i + 1];
-    }
-    if (i == 14) {
-        lightHex = hex[i] + hex[i + 1];
-    }
-    if (i == 16) {
-        tsrHex = hex[i] + hex[i + 1];
-    }
-}
-
 function calVolt(voltHex) {
-    decVolt = hexToDec(voltHex);
+    var decVolt = hexToDec(voltHex);
     return (3.0 * decVolt / 4095);
 }
 
 function calTemp(tempHex) {
-    decTemp = hexToDec(tempHex);
-    calTemp = ((-39.6) + (0.01 * decTemp))
-    return calTemp;
+    var decTemp = hexToDec(tempHex);
+    return ((-39.6) + (0.01 * decTemp));
 }
 
 function calHum(humHex, temp) {
-    decHum = hexToDec(humHex);
-    RHlinear = -2.0468 + (0.0367 * decHum) + ((-1.5955 / 1000000) * decHum * decHum);
-    RHtrue = ((temp - 25) * (0.01 + (0.00008 * decHum))) + RHlinear;
-    return RHtrue;
+    var decHum = hexToDec(humHex);
+    var RHlinear = -2.0468 + (0.0367 * decHum) + ((-1.5955 / 1000000) * decHum * decHum);
+    return ((temp - 25) * (0.01 + (0.00008 * decHum))) + RHlinear;
 }
 
 function calLight(lightHex) {
-    decLight = hexToDec(lightHex);
+    var decLight = hexToDec(lightHex);
     return (1.5 * decLight / 4096) * 6250;
 }
 
 function calTSR(tsrHex) {
-    decTSR = hexToDec(tsrHex);
+    var decTSR = hexToDec(tsrHex);
     return (1.5 * decTSR / 4096) * 769;
 }
 
-
-
-console.log(calVolt(voltHex));
-console.log(calTemp(tempHex));
-console.log(calHum(humHex, calTemp));
-console.log(calLight(lightHex));
-console.log(calTSR(tsrHex));
+module.exports = {
+    parse
+};
